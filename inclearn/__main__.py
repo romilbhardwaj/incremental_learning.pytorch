a18d3bea5f366eddce668709d5136c34b52126e3
romilbhardwaj <romil.bhardwaj@gmail.com>
2019-05-30 09:22:39 -0700

    Add __name__ to fix pytorch multiprocessing

--- inclearn/__main__.py
+++ inclearn/__main__.py
@@ -1,10 +1,11 @@
 from inclearn import parser
 from inclearn.train import train
 
-args = parser.get_parser().parse_args()
-args = vars(args)  # Converting argparse Namespace to a dict.
+if __name__ == '__main__':
+    args = parser.get_parser().parse_args()
+    args = vars(args)  # Converting argparse Namespace to a dict.
 
-if args["seed_range"] is not None:
-    args["seed"] = list(range(args["seed_range"][0], args["seed_range"][1] + 1))
+    if args["seed_range"] is not None:
+        args["seed"] = list(range(args["seed_range"][0], args["seed_range"][1] + 1))
 
-train(args)
+    train(args)
